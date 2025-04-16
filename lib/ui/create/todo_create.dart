4612ef332b02f2709e37b59edbe65e80aa994a1d
leehyunjin <skyhyunjinlee@gmail.com>
2025-04-16 21:14:13 +0900

    feat: 글 작성 UI 구현

--- lib/ui/create/todo_create.dart
+++ lib/ui/create/todo_create.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
-class TodoCreate extends StatelessWidget {
+class TodoCreate extends StatefulWidget {
   const TodoCreate({super.key});
 
   @override
+  State<TodoCreate> createState() => _TodoCreateState();
+}
+
+class _TodoCreateState extends State<TodoCreate> {
+  @override
   Widget build(BuildContext context) {
-    return Text('TodoCreate');
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('글 쓰기'),
+          actions: [
+            GestureDetector(
+              onTap: () {
+                print('글 쓰기 완료 버튼');
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  '완료',
+                  style: TextStyle(fontSize: 18, color: Colors.blue),
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(hintText: '제목을 적어주세요.'),
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: TextField(
+                        textInputAction: TextInputAction.newline,
+                        expands: true,
+                        maxLines: null,
+                        decoration: InputDecoration(hintText: '할 일을 적어주세요'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
