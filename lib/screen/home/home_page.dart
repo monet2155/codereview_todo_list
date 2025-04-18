5d768a85c5ab341988552d54d78f56c11f1e74e8
ilsong963 <ilsong963@naver.com>
2025-04-18 20:07:41 +0900

    remove: 중복 home 삭제

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:codereview_todo_list/screen/home/widgets/todo_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,10 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Column(children: [TodoCardWidget(todo: "test1")]));
   }
 }
-
